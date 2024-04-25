d66cf827227b5aa5ab236bffa82779324da788e2
Aethril <sebthar@gmail.com>
2024-04-25 23:45:59 +0200

    fixing transaction issue

--- migrations/Version20240424064632.php
+++ migrations/Version20240424064632.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240424064631 extends AbstractMigration
+final class Version20240424064632 extends AbstractMigration
 {
     public function getDescription(): string
     {
