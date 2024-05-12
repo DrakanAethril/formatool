fa879ac75825f82eb22e4ac3c8eb3a19fc0f8bcc
Aethril <sebthar@gmail.com>
2024-05-12 08:52:43 +0200

    quick fix use missing in training ajax controller for mapentity

--- src/Controller/TrainingsAjaxController.php
+++ src/Controller/TrainingsAjaxController.php
@@ -12,6 +12,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
+
 #[Route('/app')]
 class TrainingsAjaxController extends AbstractController
 {
